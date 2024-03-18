0b29ee134fc51c9b7cb9fb10e91a5ba3e93a7f10
s26621 <S26621@pjwstk.edu.pl>
2024-03-18 11:27:38 +0100

    Podstawowe klasy kontener i kontenerowiec

---
+++ APBD 3/APBD 3/Models/Kontener.cs
@@ -0,0 +1,89 @@
+﻿namespace APBD_3;
+
+public abstract class Kontener
+{
+    protected int wysokosc;
+    protected int masaLadunku;
+    protected int masaKontenera;
+    protected int glebokosc;
+    protected int numer;
+    protected static int ktoryKontener = 1;
+    protected int maksLadownosc;
+
+    public Kontener(int wysokosc, int masaKontenera, int glebokosc, int maksLadownosc)
+    {
+        this.wysokosc = wysokosc;
+        this.masaKontenera = masaKontenera;
+        this.glebokosc = glebokosc;
+        this.maksLadownosc = maksLadownosc;
+        masaLadunku = 0;
+        numer = ktoryKontener++;
+    }
+
+    public int Wysokosc
+    {
+        get => wysokosc;
+        set => wysokosc = value;
+    }
+
+    public int MasaKontenera
+    {
+        get => masaKontenera;
+        set => masaKontenera = value;
+    }
+
+    public int Glebokosc
+    {
+        get => glebokosc;
+        set => glebokosc = value;
+    }
+
+    public int MaksLadownosc
+    {
+        get => maksLadownosc;
+        set => maksLadownosc = value;
+    }
+
+ 
+
+    public int MasaLadunku
+    {
+        get => masaLadunku;
+        set => masaLadunku = value;
+    }
+
+    private int Numer
+    {
+        get => numer;
+        set => numer = value;
+    }
+
+    public void Zaladuj(int waga)
+    {
+        if (masaLadunku + waga> MaksLadownosc)
+                throw new OverfillException("Masa ladunku przekracza maksymalna pojemnosc! Nie zaladowano statku.");
+            
+        masaLadunku += waga;
+            
+    }
+
+    public void Wyladuj()
+    {
+        masaLadunku = 0;
+    }
+
+    public void Wyladuj(int ile)
+    {
+        masaLadunku -= ile;
+        if (masaLadunku < 0) masaLadunku = 0;
+    }
+}
+
+
+
+public class OverfillException : Exception
+{
+    public OverfillException(string? message) : base(message)
+    {
+    }
+}
---
+++ APBD 3/APBD 3/Models/Kontenerowiec.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_3;
+
+public class Kontenerowiec
+{
+    private List<Kontener> kontenery;
+    private string nazwa;
+
+    public Kontenerowiec(string nazwa)
+    {
+        this.nazwa = nazwa;
+    }
+}
