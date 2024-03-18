275204891d3f7e7003a40bdbc2b2970f26648bf4
s26621 <S26621@pjwstk.edu.pl>
2024-03-18 11:45:21 +0100

    Troche kontenera na plyny

---
+++ APBD 3/APBD 3/Models/IHazardNotifier.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_3;
+
+public interface IHazardNotifier
+{
+    public string NotyfikacjaTekstowa(string typSytuacji, int numer)
+    {
+        return "Zaszla niebezpieczna sytuacja: " + typSytuacji + " w kontenerze numer " + numer;
+    }
+}
---
+++ APBD 3/APBD 3/Models/KontenerNaPlyny.cs
@@ -0,0 +1,16 @@
+﻿namespace APBD_3;
+
+public class KontenerNaPlyny : Kontener, IHazardNotifier
+{
+    public KontenerNaPlyny(int wysokosc, int masaKontenera, int glebokosc, int maksLadownosc) : base(wysokosc, masaKontenera, glebokosc, maksLadownosc)
+    {
+    }
+
+    public void Zaladuj(int waga)
+    {
+        if (masaLadunku + waga > maksLadownosc * 0.9)
+        {
+            // tutaj wyswietlamy informacje
+        }
+    }
+}
